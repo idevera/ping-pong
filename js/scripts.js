b93e9b32b64925b18f2ae5093accc269ef689ee1
Irene <irenneeyyy@gmail.com>
2017-03-12 00:46:30 -0800

    Add jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,22 @@
 $(document).ready(function() {
 	$("form").submit(function(event) {
-
+		event.preventDefault();
+			alert("Hello");
 		var number = parseInt($("input#number").val());
 
 		for (i = 1; i <= number; i++) {
-			if (i % 3 === 0) {
+			if (i % 3 === 0 && i % 5 === 0) {
+				$("ul#result").append("ping pong");
+				alert("Hello");
+			} else if (i % 3 === 0) {
 				$("ul#result").append("ping");
 			} else if (i % 5 === 0) {
 				$("ul#result").append("pong");
-			} else if (i % 3 === 0 && i % 5 === 0) {
-				$("ul#result").append("pingpong");
 			} else {
 				$("ul#result").append(i);
 			}
 		}
 
-		event.preventDefault();
 	});
 });
 

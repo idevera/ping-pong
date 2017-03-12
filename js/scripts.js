603cf98bb5f40a68dfacb51b9a2e37334a6861d5
Irene <irenneeyyy@gmail.com>
2017-03-12 00:00:33 -0800

    Add more javascript

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,16 @@ $(document).ready(function() {
 
 		for (i = 1; i <= number; i++) {
 			if (i % 3 === 0) {
-				$("ul#result").append(i);
+				$("ul#result").append("ping");
+			} else if (i % 5 === 0) {
+				$("ul#result").append("pong");
+			} else if (i % 15 === 0)
+				$("ul#result").append("pingpong");
 			} else {
-				alert("Hello");
+
 			}
 		}
+
 		event.preventDefault();
 	});
 });
